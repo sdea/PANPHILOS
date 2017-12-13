24e6a11b4973b0b8a15e6d28fec78d456bd89cbd
sdea <sdea@dtu.dk>
2017-12-13 17:26:13 +0100

    Corrected Relative Header Path

--- src/io/InputOutput.h
+++ src/io/InputOutput.h
@@ -1,8 +1,8 @@
 #ifndef INPUTOUTPUT_H_INCLUDED
 #define INPUTOUTPUT_H_INCLUDED
 
-#include "../src/physics/ComputeSegmentation.h"
-#include "../src/physics/ComputeSegmentation.hh"
+#include "../physics/ComputeSegmentation.h"
+#include "../physics/ComputeSegmentation.hh"
 
 /* Header file with classes and functions for input-output */ 
 
