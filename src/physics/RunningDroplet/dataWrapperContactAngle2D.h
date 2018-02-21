3ca654ce3ccfa3d5e6d6466a4a0711a46c700543
Martina Trini <mtrini@aries.local>
2018-02-21 09:13:42 -0500

    Adding the folder for droplet with different contac angles

---
+++ src/physics/RunningDroplet/dataWrapperContactAngle2D.h
@@ -0,0 +1,23 @@
+#include "./ComputeSegmentation2D.h"
+#include "./ComputeSegmentation2D.h"
+#include "./solveSBMProcessorContactAngle2D.h"
+#include "./solveSBMProcessorContactAngle2D.hh"
+#include "./ApplyNoFlux2DFunctional.h"
+#include "./ApplyNoFlux2DFunctional.hh"
+#include "../io/InputOutput.h"
+
+namespace plb {
+// === List of wrapper functions around data processors 
+template<typename T>
+void compute_SBM_CH_step_contact_angle_2D(std::vector<MultiBlock2D* >& blocksForSim, Box2D dmn, SimulationParameter2D<T>& param) {
+	
+     // Apply processing functional on the blocks
+     T eps2 = param.eps*param.eps;
+     T invH2 = 1./(param.h*param.h);
+     applyProcessingFunctional(new solveSBMContactAngle2D<T>(eps2, param.Q, invH2, param.dt, param.M , param.h, param.cosTheta, param.SMobility, param.useG), dmn , blocksForSim);
+
+}
+
+
+
+}
