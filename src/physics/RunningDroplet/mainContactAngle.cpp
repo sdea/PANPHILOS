18da21a135343e68fff8d310bb6bceaeca90306e
mtri <mtri@dtu.dk>
2018-02-22 01:08:38 +0100

    Running droplet folder: data processor,dataWrapper,InputOutput,main,geometries and xml file

---
+++ src/physics/RunningDroplet/mainContactAngle.cpp
@@ -0,0 +1,124 @@
+// Includes for 2D version of the code
+#include "palabos2D.h"
+#include "palabos2D.hh"
+
+typedef double T;
+using namespace plb;
+
+#include <ctime>
+
+// Data processors
+#include "../src/io/InputOutputContactAngle.h"
+#include "../src/physics/ApplyNoFlux2DFunctional.h"
+#include "../src/physics/ApplyNoFlux2DFunctional.hh"
+#include "../src/physics/solveSBMProcessorContactAngle2D.h"
+#include "../src/physics/solveSBMProcessorContactAngle2D.hh"
+#include "../src/physics/dataWrapperContactAngle2D.h"
+
+
+void applyNoFlux2Dfunction(MultiScalarField2D<T>& mu, MultiScalarField2D<T>& C) {
+
+     const plint nx = C.getNx();
+     const plint ny = C.getNy();
+
+     Box2D left(0, 1, 0, ny-1);
+     Box2D right(nx-2, nx-1, 0, ny-1);
+     Box2D bottom(0, nx-1, 0, 1);
+     Box2D top(0, nx-1, ny-2, ny-1);
+     
+
+     // No flux for mu
+     applyProcessingFunctional(new applyNoFlux2D<T>(0,-1), left, mu);
+     applyProcessingFunctional(new applyNoFlux2D<T>(0, 1), right, mu);
+     applyProcessingFunctional(new applyNoFlux2D<T>(1,-1), bottom, mu);
+     applyProcessingFunctional(new applyNoFlux2D<T>(1, 1), top, mu);
+
+     // No-flux for C
+     applyProcessingFunctional(new applyNoFlux2D<T>(0,-1), left, C);
+     applyProcessingFunctional(new applyNoFlux2D<T>(0, 1), right, C);
+     applyProcessingFunctional(new applyNoFlux2D<T>(1,-1), bottom, C);
+     applyProcessingFunctional(new applyNoFlux2D<T>(1, 1), top, C);
+    
+}
+
+int main(int argc, char *argv[]) {
+
+    // Init simulation here
+    plbInit(&argc, &argv);
+
+    if (argc != 2) {
+
+	pcout << "Wrong number of input parameters" << std::endl;
+	return -1;
+
+    }
+
+    SimulationParameter2D<T> param(argv[1]);
+    param.print();
+
+
+    // Allocate needed scalar field
+    MultiScalarField2D<T> mu(param.nx, param.ny);
+    MultiScalarField2D<T> C(param.nx,  param.ny);
+    MultiScalarField2D<T> Psi(param.nx, param.ny);
+    MultiScalarField2D<T> theta(param.nx, param.ny);
+    MultiScalarField2D<T> geom(param.nx, param.ny);
+
+    // Read geometries
+    readGeom2D(param.fileCName, C);
+    readGeom2D(param.filePsiName, Psi);
+    readGeom2D(param.fileThetaName, theta);
+
+    // Test
+    /*pcout << "Warning: Test for Mass" << std::endl;
+    pcout << "Psi == 1 everywhere"    << std::endl;
+    pcout << "Remove the section for real studies" << std::endl;
+    setToConstant(Psi, Psi.getBoundingBox(), 1.);*/	
+
+    // Write Inital vtks
+    std::vector<MultiBlock2D*> blocksForOutput;
+    blocksForOutput.push_back(&geom);
+    blocksForOutput.push_back(&C);
+    blocksForOutput.push_back(&Psi);
+    
+    // Blocks for simulations
+    std::vector<MultiBlock2D*> blocksForSim;
+    blocksForSim.push_back(&mu);
+    blocksForSim.push_back(&C);
+    blocksForSim.push_back(&Psi);
+    blocksForSim.push_back(&theta);
+
+    // Only for test
+    writeSingleVTK2D<T>(Psi, "PsiIni", 0, 0);
+
+
+    // Domain of the simulation
+    Box2D dmn(1, param.nx -2, 1, param.ny -2);
+    
+    clock_t begin = clock();
+    for (plint iTT=param.initIter; iTT < param.maxIter; ++iTT) {
+
+        pcout << "Iteration:  " << iTT << std::endl;
+	    
+	if (iTT%param.freqOut == 0) {
+            	
+		// Output
+            	pcout << "Printing output:  " << iTT << std::endl;
+        	writeDat2D<T>(C, param.outDatName, iTT, 15);
+        	writeFullVTK2D<T>(blocksForOutput, C.getBoundingBox() , param.outVTKName, iTT, 15);
+	   }
+
+	// Cahn-Hillard step
+	compute_SBM_CH_step_contact_angle_2D<T>(blocksForSim, dmn, param);
+        
+	// Boundary conditions
+        applyNoFlux2Dfunction(mu, C);
+    }
+    
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    pcout << "Elapsed time:   " << elapsed_secs << std::endl;
+
+    pcout << "Hello palabos!!!" << std::endl;
+    return 0;
+}
---
+++ src/physics/RunningDroplet/solveSBMProcessorContactAngle2D.hh
@@ -0,0 +1,156 @@
+#ifndef SOLVESBMPROCESSORCONTACTANGLE2D_HH_INCLUDED
+#define SOLVESBMPROCESSORCONTACTANGLE2D_HH_INCLUDED
+
+#include "solveSBMProcessorContactAngle2D.h"
+
+template<typename T>
+solveSBMContactAngle2D<T>::solveSBMContactAngle2D(T eps2_, T Q_, T invh2_, T dt_, T M_, T h_, T cosTheta_, bool SM_, bool useG_):
+                    eps2(eps2_), Q(Q_), invh2(invh2_), dt(dt_), M(M_), h(h_), cosTheta(cosTheta_), SM(SM_),
+		    useG(useG_)	{ }
+
+// Override generic process method
+template<typename T>
+void solveSBMContactAngle2D<T>::processGenericBlocks(Box2D domain, std::vector<AtomicBlock2D* > atomicBlocks) {
+
+
+     // Casting back the blocks to their specific type
+     ScalarField2D<T>& mu = *dynamic_cast<ScalarField2D<T>* >(atomicBlocks[0]);
+     ScalarField2D<T>& C = *dynamic_cast<ScalarField2D<T>* >(atomicBlocks[1]);
+     ScalarField2D<T>& Psi = *dynamic_cast<ScalarField2D<T>* >(atomicBlocks[2]);
+     ScalarField2D<T>& theta = *dynamic_cast<ScalarField2D<T>* >(atomicBlocks[3]);
+     
+    // Compute displacments
+     Dot2D offset_C = computeRelativeDisplacement(mu, C);
+     Dot2D offset_Psi = computeRelativeDisplacement(mu, Psi);
+     Dot2D offset_theta = computeRelativeDisplacement(mu, theta);
+
+     // Loop over the indexes of the blocks
+     for (plint iX=domain.x0; iX<=domain.x1; ++iX) {
+
+         // X Displacements for block C and dPsiOvPsi
+         plint iX_C = iX + offset_C.x;
+         plint iX_Psi = iX + offset_Psi.x;
+	 plint iX_theta = iX + offset_theta.x;
+
+
+         for (plint iY=domain.y0; iY<=domain.y1; ++iY) {
+
+            // Y Displacements for block C and dPsiOvPsi
+            plint iY_C = iY + offset_C.y;
+            plint iY_Psi = iY + offset_Psi.y;
+	    plint iY_theta = iY + offset_theta.y;
+
+
+                // Compute dPsiOvPsi
+                T dxPsi = (Psi.get(iX_Psi +1, iY_Psi) - Psi.get(iX_Psi -1, iY_Psi))/(2.*h);
+                T dyPsi = (Psi.get(iX_Psi, iY_Psi +1) - Psi.get(iX_Psi, iY_Psi -1))/(2.*h);
+
+		T dPx = dxPsi/Psi.get(iX_Psi,iY_Psi);
+		T dPy = dyPsi/Psi.get(iX_Psi,iY_Psi);
+
+                // Compute modGPsiOvPsi
+                T  modGPsiOvPsi = std::sqrt(dPx*dPx + dPy*dPy);
+
+                // Update Mu ******************************************************************
+                T laplacianC = invh2*(C.get(iX_C +1, iY_C) + C.get(iX_C -1,iY_C)   +
+                                      C.get(iX_C ,iY_C +1) + C.get(iX_C, iY_C -1)  -
+                                      4.*C.get(iX_C, iY_C));
+
+
+                // Get derivative of C
+                T dxC = (C.get(iX_C +1, iY_C) - C.get(iX_C -1,iY_C))/(2.*h);
+                T dyC = (C.get(iX_C ,iY_C +1) - C.get(iX_C, iY_C -1))/(2.*h);
+
+
+                T dotdC_dPsiOvPsi = (dPx*dxC + dPy*dyC);
+
+                // Derivative of energy funcitonal
+                T F = (Q/4.)*(C.get(iX_C,iY_C))*(C.get(iX_C,iY_C))*
+                             (1 - C.get(iX_C,iY_C))*(1 - C.get(iX_C,iY_C));
+                T dF = (Q/2.)*C.get(iX_C,iY_C)*(1. - C.get(iX_C,iY_C))*
+                                                                (1. - 2.*C.get(iX_C,iY_C));
+
+                mu.get(iX,iY) = dF- eps2*(dotdC_dPsiOvPsi + laplacianC) -
+                                        std::sqrt(eps2)*
+                                        modGPsiOvPsi*
+                                        std::sqrt(2*F)*theta.get(iX_theta,iY_theta);
+                
+                // *****************************************************************************
+
+                // Get derivatives of mu
+                T dxMu = (mu.get(iX +1, iY) - mu.get(iX -1,iY))/(2.*h);
+                T dyMu = (mu.get(iX ,iY +1) - mu.get(iX, iY -1))/(2.*h);
+
+
+                T laplacianMu = invh2*(mu.get(iX +1, iY) + mu.get(iX -1,iY)   +
+                                      mu.get(iX ,iY +1) + mu.get(iX, iY -1) -
+                                      4.*mu.get(iX, iY));
+
+                T dotdMu_dPsiOvPsi = (dPx*dxMu + dPy*dyMu);
+
+                // Update C
+                if(SM == false){
+                	
+			C.get(iX_C,iY_C) += M*dt*(dotdMu_dPsiOvPsi + laplacianMu);
+		}
+		else {
+
+			// Mobility and its derivative
+			T Mc = M*(C.get(iX_C,iY_C))*(C.get(iX_C,iY_C))*
+				(1 - C.get(iX_C,iY_C))*(1 - C.get(iX_C,iY_C));
+			T dMc = 2*M*C.get(iX_C,iY_C)*(1. - C.get(iX_C,iY_C))*
+				(1. - 2.*C.get(iX_C,iY_C));
+
+			// Default (useG == false)
+			T G = 1.;
+			T dG = 0.;
+
+			if (useG == true) {
+				
+				// Compute the polinomial function G and its deivatives
+				T psi = Psi.get(iX_Psi,iY_Psi);
+				T psi6 = std::pow(psi, 6);
+				T psi5 = std::pow(psi, 5);
+				T psi2 = psi*psi;
+
+				// G function
+				G = psi6*(10.*psi2 - 15.*psi + 6.);
+				dG = psi5*(80.*psi2 - 105.*psi + 36.);
+
+			} 
+
+			// Derivatives of surface mobility
+			T dxM = G*dMc*dxC + Mc*dG*dxPsi;
+			T dyM = G*dMc*dyC + Mc*dG*dyPsi;
+
+			// Dot produt dM * dMu
+			T dotdMdMu = dxM*dxMu + dyM*dyMu;
+
+			// Update C
+			C.get(iX_C,iY_C) += dt*(Mc*G*dotdMu_dPsiOvPsi + dotdMdMu +
+								Mc*G*laplacianMu);
+                }
+         }
+     } // end for
+}
+
+template<typename T>
+solveSBMContactAngle2D<T>*
+        solveSBMContactAngle2D<T>::clone() const {
+
+    return new solveSBMContactAngle2D<T>(*this);
+}
+
+ 
+template<typename T>
+void solveSBMContactAngle2D<T>::getTypeOfModification(std::vector<modif::ModifT>& modified) const {
+
+    modified[0] = modif::staticVariables;
+    modified[1] = modif::staticVariables;
+    modified[2] = modif::nothing;
+    modified[3] = modif::nothing;
+
+}
+
+
+#endif // SOLVESBMPROCESSORCONTACTANGLE2D_HH_INCLUDED
